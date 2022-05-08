27f681e88ef00cd26339e2d127ffc24200b12446
cacheninetynine <88933951+cacheninetynine@users.noreply.github.com>
2022-05-08 17:23:07 +0300

    Updated main.py to the latest noplz version

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ def noplz(esotericcode):
             if vsmode:
                 v = v + 1
                 if v > 26 and vsmode:
-                    return "noplz error: Text variable over 26! Maybe you forgot to reset the variable?"
+                    v = 26
+                    vs = num2alpha[v]
                 else:
                     if v == 0:
                         vs = " "
@@ -32,7 +33,8 @@ def noplz(esotericcode):
             if vsmode:
                 v = v - 1
                 if v < 0 and vsmode:
-                    return "noplz error: Text variable is negative!"
+                    v = 0
+                    vs = num2alpha[v]
                 else:
                     if v == 0:
                         vs = " "
@@ -61,7 +63,8 @@ def noplz(esotericcode):
             if vsmode:
                 v = v * 2
                 if v > 26 and vsmode:
-                    return "noplz error: Text variable over 26! Maybe you forgot to reset the variable?"
+                    v = 26
+                    vs = num2alpha[v]
                 else:
                     if v == 0:
                         vs = " "
@@ -73,7 +76,9 @@ def noplz(esotericcode):
             asciimode = True
         ei = ei + 1
 
-# you can delete everything below, it's the UI
+
+# you can delete everything below, its the ui
+
 
 def clicked():
     txt2.config(text = noplz(txt.get()))
@@ -92,18 +97,3 @@ txt.grid(column=1, row=0)
 btn = Button(window, text="Run the code", command=clicked)
 btn.grid(column=2, row=0)
 window.mainloop()
-
-# small documentation
-# c : creates (makes it look like it created it) an empty variable (resets it to 0 or " " (if text mode) if already created)
-# + : adds 1 to the variable (or moves one letter further if string mode enabled)
-# - : removes 1 to the variable (or moves one letter back if string mode enabled)
-# t : turns the variable to string (text) mode (disabled by default)
-# p : prints the current variable
-# s : prints the source code
-# * : adds current variable to the mix variable
-# ! : prints the mix variable
-# % : multiplies the variable by 2
-# a : ascii text mode (disabled by default too and does nothing (yet))
-
-# examples:
-# print all numbers from 1 to 9: c+*+*+*+*+*+*+*+*+*!
